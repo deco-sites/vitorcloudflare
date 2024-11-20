737f809247d638d945ad13810c28d61d72b3088b
Vitor Ribeiro <vitor.ribeiro@deco.cx>
2024-11-20 00:17:28 +0000

    Publish 3 from staging into origin/main

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -41,50 +41,6 @@ export interface Props {
 }
 
 export default function Footer({
-  logo = {
-    src:
-      "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/67120bcd-936a-4ea5-a760-02ed5c4a3d04",
-    alt: "Logo",
-  },
-  links = [
-    {
-      title: "Column One",
-      items: [
-        { label: "Link One", href: "/" },
-        { label: "Link Two", href: "/" },
-        { label: "Link Three", href: "/" },
-        { label: "Link Four", href: "/" },
-        { label: "Link Five", href: "/" },
-      ],
-    },
-    {
-      title: "Column Two",
-      items: [
-        { label: "Link Six", href: "/" },
-        { label: "Link Seven", href: "/" },
-        { label: "Link Eight", href: "/" },
-        { label: "Link Nine", href: "/" },
-        { label: "Link Ten", href: "/" },
-      ],
-    },
-    {
-      title: "Column Three",
-      items: [
-        { label: "Link Eleven", href: "/" },
-        { label: "Link Twelve", href: "/" },
-        { label: "Link FourThirteenteen", href: "/" },
-        { label: "Link Fourteen", href: "/" },
-        { label: "Link Fifteen", href: "/" },
-      ],
-    },
-  ],
-  subscribe = {
-    title: "Subcribe",
-    description:
-      "Join our newsletter to stay up to date on features and releases.",
-    instructions:
-      "By subscribing you agree to with our <a href='/' target='_blank' class='link'>Privacy Policy</a> and provide consent to receive updates from our company.",
-  },
   madeWith = {
     label: "Made with",
     src:
@@ -108,56 +64,6 @@ export default function Footer({
   return (
     <div class="lg:container mx-auto md:max-w-6xl px-4 pt-16 text-sm">
       <div class="flex flex-col gap-20">
-        <div class="flex flex-col gap-6 justify-between lg:flex-row">
-          <div>
-            <Image
-              src={logo.src || ""}
-              width={100}
-              height={28}
-              alt={logo.alt}
-            />
-          </div>
-          <div class="flex gap-9">
-            {links?.map((link) => (
-              <div>
-                <h4 class="font-semibold mb-4">{link.title}</h4>
-                {link.items?.map((item) => (
-                  <a
-                    class="block hover:underline link no-underline py-1"
-                    href={item.href}
-                  >
-                    {item.label}
-                  </a>
-                ))}
-              </div>
-            ))}
-          </div>
-          <div class="lg:w-[40%]">
-            <h4 class="font-semibold mb-4">{subscribe?.title}</h4>
-            <form class="flex flex-col gap-4">
-              <p class="font-normal">{subscribe.description}</p>
-              <div class="flex gap-4">
-                <input
-                  type="text"
-                  placeholder="Enter your email"
-                  class="w-full input input-bordered input-primary"
-                />
-                <button
-                  type="submit"
-                  class="btn btn-outline font-normal"
-                  aria-label="Subscribe"
-                >
-                  Subscribe
-                </button>
-              </div>
-              <p
-                class="text-xs"
-                dangerouslySetInnerHTML={{ __html: subscribe.instructions }}
-              >
-              </p>
-            </form>
-          </div>
-        </div>
         <div class="border-primary border-t flex flex-col gap-4 items-center justify-between lg:flex-row lg:items-center py-8">
           <div class="flex flex-col gap-4 items-center lg:flex-row lg:gap-6">
             <a
